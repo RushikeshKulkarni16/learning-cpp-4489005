75840830f4063fdd7a5cfe47a366273edad2a0de
RushikeshKulkarni16 <rushikeshtkulkarni@gmail.com>
2024-12-25 17:12:21 +0000

    Cin

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,10 @@
 #include <string>
 
 int main(){
+    std::cout<<"Enter your name: " << std::flush;
     std::string str;
     std::cin >> str;
-    std::cout << str;
+    std::cout<<"Nice to meet you " << str <<"!"<< std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
